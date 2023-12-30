b917f2efcbe274cdd10ace0708eb993a68e58170
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2023-12-30 13:33:07 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Sorting a numeric array and a string array
+import java.util.Arrays;
+public class Main{
+  public static void main(String[] args){
+    //Sorting of Numbers
+    int[] array = {4,5,23,232,23,34,57,33};
+    System.out.println("Original Array: "+Arrays.toString(array));
+    Arrays.sort(array);
+    System.out.println("Sorted Array: "+Arrays.toString(array));
+    //Sorting of Strings
+    String[] array1 = {"dharshan","durga","chintu"};
+    System.out.println("Original Array: "+Arrays.toString(array1));
+    Arrays.sort(array1);
+    System.out.println("Sorted Array: "+Arrays.toString(array1));
+  }
+}
