32192c011ca3bf536689f68b2483a1299aed0d30
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2023-12-30 16:17:12 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,10 @@
+public class Program2{
+  public static void main(String[] args){
+    int array[] = {45,34,3,23,56,76,43};
+    int sum = 0;
+    for(int i : array){
+      sum += i;
+    }
+    System.out.println("The Sum is: "+ sum);
+  }
+}
